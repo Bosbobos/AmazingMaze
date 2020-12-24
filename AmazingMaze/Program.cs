d755704569b57cdb293b7bc3f3f50d5a155f3a85
Bosbobos <konovalov.matvej@mail.ru>
2020-12-24 19:32:38 +0300

    Добавьте файлы проекта.

---
+++ AmazingMaze/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+const int worldSize = 5;
+int[,] mas = new int[worldSize, worldSize]  // Инициализируем двумерный массив 2 на 3
+{ 
+        { 0, 1, 2, 3, 4 },
+        { 0, 1, 2, 3, 4 },   
+        { 0, 1, 2, 3, 4 },    
+        { 0, 1, 2, 3, 4 },    
+        { 0, 1, 2, 3, 4 }
+};
+
+string world = @$"
+████████████████████
+███████        █████
+███████  ████  █████
+███████  ████  █████
+███████  ████  █████
+█        ████  █████
+█11██████████  █████
+█  ██████████  █████
+█              █████
+████████████████████
+";
+Console.WriteLine(world);
+int rows = mas.GetUpperBound(0) + 1;//
+int columns = mas.Length / rows;
+
+WorldObjects[,] ConvertToWorld(string template)
+{
+    string[] words = template.Split(Environment.NewLine);
+
+}
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{mas[i, j]} \t");
+    }
+    Console.WriteLine();
+}
---
+++ AmazingMaze/WorldObjects.cs
@@ -0,0 +1,7 @@
+﻿enum WorldObjects
+{
+    Wall,
+    EmptySpace,
+    Character,
+    Crystall
+}
