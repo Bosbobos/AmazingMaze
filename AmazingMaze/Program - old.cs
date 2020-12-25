36df56f6fa105ba59f3b5e3fd0d1e6da18fff524
Bosbobos <konovalov.matvej@mail.ru>
2020-12-25 19:59:41 +0300

    Сделали функцию отрисовки

---
+++ AmazingMaze/Program - old.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//const int worldSize = 5;
+//int[,] mas = new int[worldSize, worldSize]  // Инициализируем двумерный массив 2 на 3
+//{ 
+//        { 0, 1, 2, 3, 4 },
+//        { 0, 1, 2, 3, 4 },   
+//        { 0, 1, 2, 3, 4 },    
+//        { 0, 1, 2, 3, 4 },    
+//        { 0, 1, 2, 3, 4 }
+//};
+
+//int rows = mas.GetUpperBound(0) + 1;// Количество строк
+//int columns = mas.Length / rows;
+
+//string world = @$"
+//████████████████████
+//███████        █████
+//███████  ████  █████
+//███████  ████  █████
+//███████  ████  █████
+//█        ████  █████
+//█11██████████  █████
+//█  ██████████  █████
+//█              █████
+//████████████████████
+//";
+//Console.WriteLine(world);
+//WorldObjects[,] ConvertToWorld(string template)// Метод, принимабщий строку и выводящий массив enum'ов WorldObjexts
+//{
+//    string[] words = template.Split(Environment.NewLine);// Разбивает нашу строку, каждая строчка - новый массив, В итоге получается двумерный массив
+
+//}
+
+//for (int i = 0; i < rows; i++)
+//{
+//    for (int j = 0; j < columns; j++)
+//    {
+//        Console.Write($"{mas[i, j]} \t");
+//    }
+//    Console.WriteLine();
+//}
--- AmazingMaze/Program.cs
+++ AmazingMaze/Program.cs
@@ -1,41 +1,88 @@
-﻿using System;
-const int worldSize = 5;
-int[,] mas = new int[worldSize, worldSize]  // Инициализируем двумерный массив 2 на 3
-{ 
-        { 0, 1, 2, 3, 4 },
-        { 0, 1, 2, 3, 4 },   
-        { 0, 1, 2, 3, 4 },    
-        { 0, 1, 2, 3, 4 },    
-        { 0, 1, 2, 3, 4 }
-};
+﻿/*
+ * спрайты: █ ▓ 00
+ * 
+ * задачи:
+ *    красивый редактор уровней
+ *    красивенькие спрайтики
+*/
 
-int rows = mas.GetUpperBound(0) + 1;// Количество строк
-int columns = mas.Length / rows;
+using System;
 
-string world = @$"
-████████████████████
-███████        █████
-███████  ████  █████
-███████  ████  █████
-███████  ████  █████
-█        ████  █████
-█11██████████  █████
-█  ██████████  █████
-█              █████
-████████████████████
-";
-Console.WriteLine(world);
-WorldObjects[,] ConvertToWorld(string template)// Метод, принимабщий строку и выводящий массив enum'ов WorldObjexts
+// переменная, в которой лежит мир
+const int worldSize = 3;
+WorldObjects[,] world = new WorldObjects[worldSize, worldSize];
+
+// переменная, в которой лежат очки персонажа
+int points = 0; 
+
+// функция отрисовки мира
+void renderView()
 {
-    string[] words = template.Split(Environment.NewLine);// Разбивает нашу строку, каждая строчка - новый массив, В итоге получается двумерный массив
+    string pointsView = $"Очки: {points}" + Environment.NewLine;
+    Console.WriteLine(pointsView);
+    
+    string worldView = worldToString(world);
+    Console.WriteLine(worldView);
 
 }
 
-for (int i = 0; i < rows; i++)
+string worldToString(WorldObjects[,] worldObjects)
 {
-    for (int j = 0; j < columns; j++)
+    string result = "";
+
+    for (int j = 0; j < worldSize; j++)
     {
-        Console.Write($"{mas[i, j]} \t");
+        for (int i = 0; i < worldSize; i++)
+        {
+            result += worldObjectToString(worldObjects[j, i]);
+        }
+        result += Environment.NewLine;
     }
-    Console.WriteLine();
+
+    return result;
 }
+
+string worldObjectToString(WorldObjects worldObjects)
+{
+    switch (worldObjects)
+    {
+        case WorldObjects.Wall:
+            return "██";
+        case WorldObjects.EmptySpace:
+            return "  ";
+        case WorldObjects.Character:
+            return "▓▓";
+        case WorldObjects.Crystall:
+            return "00";
+        default:
+            throw new NotImplementedException("Add new case to the switch");
+    }
+}
+
+// функция логики игры
+void gameLogic(ConsoleKey key)
+{
+    switch (key)
+    {
+        case ConsoleKey.RightArrow:
+            break;
+        case ConsoleKey.LeftArrow:
+            break;
+        case ConsoleKey.UpArrow:
+            break;
+        case ConsoleKey.DownArrow:
+            break;
+        default:
+            break;
+    }
+}
+
+//      определение нажатой клавиши(switch)
+//      проверка столкновения с бонусом
+
+renderView();
+
+// функция перехвата нажатия клавиш
+var key = Console.ReadKey();
+
+gameLogic(key.Key);
